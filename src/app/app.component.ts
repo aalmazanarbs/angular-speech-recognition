90048116236799ce2f4bec188f9708e8da25d537
Alejandro Almazán <aalmazan@autentia.com>
2019-08-08 16:40:19 +0200

    Add listening animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { tap, takeWhile } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    shadowleft: any = '-69px';
+    shadowtop: any = '-68px';
     message = 'angular-speech-recognition sahdkjashjdkashkj ahskd askhajkshdk asdaskaskaskjskhakshkasjh kjashkjaskhaskjdakjhkd';
+    borderheight = 0;
+    listening = true;
+
+    ngOnInit(): void {
+        timer(0, 400)
+            .pipe(takeWhile(() => this.listening === true))
+            .subscribe(() => this.borderheight = this.randomize(0.6, 1));
+    }
+
+    randomize(min, max) {
+        let x;
+        x = (Math.random() * (max - min) + min);
+        return x;
+    }
 }
